00c17c5734ddf859ebc70d8dde2ae545cf78b44f
TongSir <738938205@qq.com>
2020-07-31 17:14:10 +0800

    完成了主页功能的开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -2,5 +2,23 @@ var baseURL = 'http://ajax.frontend.itheima.net'
 
 $.ajaxPrefilter(function(options) {
   options.url = baseURL + options.url
-  console.log(options)
+
+  // 统一为有权限的接口  设置headers请求头
+  if(options.url.indexOf('/my/') !==-1) {
+    options.headers = {
+      Authorization : localStorage.getItem('token') || ''
+    }
+  }
+ 
+// 全局统一挂载 complete 回调函数
+  options.complete = function(res) {
+       //在complete回调函数中 可以使用res.responseJSON拿到服务器响应回来的数据
+       if(res.responseJSON.status ===1 && res.responseJSON.message==='身份认证失败！') {
+        // 1.强制清空token
+        localStorage.removeItem('token')
+        //2.强制跳转到登录页面
+        location.href = '/login.html'
+      }
+  }
 })
+
